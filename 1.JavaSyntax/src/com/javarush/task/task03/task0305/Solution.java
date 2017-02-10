da1f814c75982ef08aacd1fc13eaaa0fdfdc24ee
YuriHeiko <yuriheiko@gmail.com>
2017-02-10 13:21:22 +0200

    task03.task0305 Solution

--- 1.JavaSyntax/src/com/javarush/task/task03/task0305/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0305/Solution.java
@@ -6,6 +6,6 @@ package com.javarush.task.task03.task0305;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        System.out.println("APRIL 5 1978");
     }
 }
