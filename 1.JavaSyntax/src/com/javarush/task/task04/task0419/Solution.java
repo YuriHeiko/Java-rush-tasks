704b924c9d0df4d8ef00f81532ecf235c65f58e0
YuriHeiko <yuriheiko@gmail.com>
2017-02-19 13:54:47 +0200

    task04.task0419 Solution

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0419/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task04.task0419;
+
+/* 
+Максимум четырех чисел
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        int c = Integer.parseInt(reader.readLine());
+        int d = Integer.parseInt(reader.readLine());
+
+        System.out.println(returnMax(returnMax(a, b), returnMax(c, d)));
+    }
+
+    private static int returnMax(int a, int b) {
+        return Math.max(a, b);
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0419/Условие.jrtc
@@ -0,0 +1,6 @@
+<div class="wdghtml" taskKey="com.javarush.task.task04.task0419">
+<h3>Максимум четырех чисел</h3>
+<p>Ввести с клавиатуры <strong>четыре числа</strong>, и вывести <strong>максимальное</strong> из них.<br />
+Если числа <strong>равны</strong> между собой, необходимо вывести <strong>любое</strong>.</p>
+
+</div>
