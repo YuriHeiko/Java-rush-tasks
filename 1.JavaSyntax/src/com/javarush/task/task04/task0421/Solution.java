126e38cdfaae20821cb3dabe26a20c764d4ec72f
YuriHeiko <yuriheiko@gmail.com>
2017-02-19 14:14:49 +0200

    task04.task0421 Solution

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0421/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task04.task0421;
+
+/* 
+Настя или Настя?
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name1 = reader.readLine();
+        String name2 = reader.readLine();
+
+        if (name1.equals(name2)) {
+            System.out.println("Имена идентичны");
+        } else if (name1.length() == name2.length()) {
+            System.out.println("Длины имен равны");
+        }
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0421/Условие.jrtc
@@ -0,0 +1,7 @@
+<div class="wdghtml" taskKey="com.javarush.task.task04.task0421">
+<h3>Настя или Настя?</h3>
+<p><strong></strong>Ввести с клавиатуры <strong>два имени</strong>, и если имена <strong>одинаковые</strong> вывести сообщение «<em><strong>Имена идентичны</strong></em>».<br />
+Если имена <strong>разные</strong>, но их длины равны – вывести сообщение – «<em><strong>Длины имен равны</strong></em>».<br />
+Если <strong>имена и длины имен разные</strong> &#8212; ничего <strong>не выводить</strong>.</p>
+
+</div>
