366370b8de918043fb7ec7e028895e26ab5aac2c
YuriHeiko <yuriheiko@gmail.com>
2017-02-19 14:10:56 +0200

    task04.task0420 Solution

--- 1.JavaSyntax/src/com/javarush/task/task04/task0420/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0420/Solution.java
@@ -4,10 +4,10 @@ package com.javarush.task.task04.task0420;
 Сортировка трех чисел
 */
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
