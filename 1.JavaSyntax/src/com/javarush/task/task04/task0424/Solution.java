926a03cab29dd48f029e7486a8f22ed0fc42e277
YuriHeiko <yuriheiko@gmail.com>
2017-02-19 14:31:11 +0200

    task04.task0424 Solution

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0424/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task04.task0424;
+
+/* 
+Три числа
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a1 = Integer.valueOf(reader.readLine());
+        int a2 = Integer.valueOf(reader.readLine());
+        int a3 = Integer.valueOf(reader.readLine());
+
+        if (a1 == a2) {
+            if (a3 != a1) {
+                System.out.println(3);
+            }
+        } else if (a2 == a3) {
+            if (a3 != a1) {
+                System.out.println(1);
+            }
+        } else if (a3 == a1) {
+            if (a3 != a2) {
+                System.out.println(2);
+            }
+        }
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0424/Условие.jrtc
@@ -0,0 +1,6 @@
+<div class="wdghtml" taskKey="com.javarush.task.task04.task0424">
+<h3>Три числа</h3>
+<p>Ввод с клавиатуры, сравнение <strong>чисел</strong> и вывод на экран — у студентов <span class="text-user"><strong>4</strong></span> уровня секретного центра <strong><span class="text-corporate">JavaRush</span></strong> эти навыки оттачиваются до автоматизма.<br />
+Давайте напишем программу, в которой пользователь <strong>вводит три</strong> <strong>числа</strong> с клавиатуры. Затем происходит <strong>сравнение</strong>, и если мы находим число, которое <span class="text-user">отличается</span> от двух других, выводим на экран его <span class="text-user">порядковый номер</span>.</p>
+
+</div>
