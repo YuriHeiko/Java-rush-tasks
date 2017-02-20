a0fba0c03a5dd045212116c2ec83d86322f29bec
YuriHeiko <yuriheiko@gmail.com>
2017-02-20 13:31:43 +0200

    task05.task0505 Solution

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0505/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task05.task0505;
+
+/* 
+Кошачья бойня
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat("C1", 3, 2, 2);
+        Cat cat2 = new Cat("C2", 2, 1, 1);
+        Cat cat3 = new Cat("C3", 3, 3, 4);
+
+        System.out.println(cat1.fight(cat2));
+        System.out.println(cat1.fight(cat3));
+        System.out.println(cat2.fight(cat3));
+    }
+
+    public static class Cat {
+        protected String name;
+        protected int age;
+        protected int weight;
+        protected int strength;
+
+        public Cat(String name, int age, int weight, int strength) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+            this.strength = strength;
+        }
+
+        public boolean fight(Cat anotherCat) {
+            int agePlus = this.age > anotherCat.age ? 1 : 0;
+            int weightPlus = this.weight > anotherCat.weight ? 1 : 0;
+            int strengthPlus = this.strength > anotherCat.strength ? 1 : 0;
+
+            int score = agePlus + weightPlus + strengthPlus;
+            return score > 2; // return score > 2 ? true : false;
+        }
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0505/Условие.jrtc
@@ -0,0 +1,8 @@
+<div class="wdghtml" taskKey="com.javarush.task.task05.task0505">
+<h3>Кошачья бойня</h3>
+<p>Создать три кота используя класс <span class="text-corporate"><strong>Cat</strong></span>.<br />
+Провести <strong>три боя</strong> попарно между котами.<br />
+Класс <span class="text-corporate"><strong>Cat</strong></span> создавать не надо. Для боя использовать метод <mark>boolean fight(Cat <strong>anotherCat</strong>)</mark>.<br />
+Результат каждого боя вывести на экран c новой строки.</p>
+
+</div>
