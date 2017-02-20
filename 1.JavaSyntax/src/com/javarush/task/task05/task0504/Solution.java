bc8e07da1844269b49fb3bbe8b6f3b185bc3c5f5
YuriHeiko <yuriheiko@gmail.com>
2017-02-20 13:30:19 +0200

    task05.task0504 Solution

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0504/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task05.task0504;
+
+
+/* 
+Трикотаж
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat("C1", 3, 2, 2);
+        Cat cat2 = new Cat("C2", 2, 1, 1);
+        Cat cat3 = new Cat("C3", 3, 3, 4);
+    }
+
+    public static class Cat {
+        private String name;
+        private int age;
+        private int weight;
+        private int strength;
+
+        public Cat(String name, int age, int weight, int strength) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+            this.strength = strength;
+        }
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0504/Условие.jrtc
@@ -0,0 +1,5 @@
+<div class="wdghtml" taskKey="com.javarush.task.task05.task0504">
+<h3>Трикотаж</h3>
+<p><strong></strong>Пару задач назад студенты секретного центра <span class="text-corporate"><strong>JavaRush</strong></span> создавали класс <strong><span class="text-corporate">Cat</span></strong>. Теперь пришла пора реализовать котов во плоти, разумеется по образу и подобию класса <span class="text-corporate"><strong>Cat</strong></span>, а точнее — основываясь на нём, как на шаблоне. Их — <strong>котов</strong> — должно быть <strong>трое</strong>. Наполните этих троих жизнью, то есть, конкретными данными.</p>
+
+</div>
