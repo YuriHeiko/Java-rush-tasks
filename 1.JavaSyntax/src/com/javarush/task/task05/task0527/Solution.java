edb56ba63dcb057b75c9957d98465afef091c3e0
YuriHeiko <yuriheiko@gmail.com>
2017-02-21 13:35:49 +0200

    task05.task0527 Solution

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0527/Solution.java
@@ -0,0 +1,51 @@
+package com.javarush.task.task05.task0527;
+
+/* 
+Том и Джерри
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Mouse jerryMouse = new Mouse("Jerry", 12, 5);
+        Cat cat = new Cat("Tom", 15, 10);
+        Dog dog = new Dog("Barboss", 20, 30);
+        //напишите тут ваш код
+    }
+
+    public static class Mouse {
+        String name;
+        int height;
+        int tail;
+
+        public Mouse(String name, int height, int tail) {
+            this.name = name;
+            this.height = height;
+            this.tail = tail;
+        }
+    }
+
+    public static class Dog {
+        String name;
+        int height;
+        int tail;
+
+        public Dog(String name, int height, int tail) {
+            this.name = name;
+            this.height = height;
+            this.tail = tail;
+        }
+    }
+
+    public static class Cat {
+        String name;
+        int height;
+        int tail;
+
+        public Cat(String name, int height, int tail) {
+            this.name = name;
+            this.height = height;
+            this.tail = tail;
+        }
+    }
+
+}
