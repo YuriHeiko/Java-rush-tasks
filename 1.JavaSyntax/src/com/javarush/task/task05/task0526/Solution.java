3610bbf2d235364d2b295d092f11aee128a7afaa
YuriHeiko <yuriheiko@gmail.com>
2017-02-21 13:33:19 +0200

    task05.task0526 Solution

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0526/Solution.java
@@ -0,0 +1,52 @@
+package com.javarush.task.task05.task0526;
+
+/* 
+Мужчина и женщина
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Man man1 = new Man("a", 1, "df");
+        Man man2 = new Man("a2", 2, "df2");
+        Woman woman1 = new Woman("b", 1, "1df");
+        Woman woman2 = new Woman("b2", 2, "1df2");
+
+        System.out.println(man1.name + " " + man1.age + " " + man1.address);
+        System.out.println(man2.name + " " + man2.age + " " + man2.address);
+        System.out.println(woman1.name + " " + woman1.age + " " + woman1.address);
+        System.out.println(woman2.name + " " + woman2.age + " " + woman2.address);
+    }
+
+    public static class Man {
+        String name;
+        int age;
+        String address;
+
+        public Man(String name, int age, String address) {
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+
+        @Override
+        public String toString() {
+            return name + " " + age + " " + address;
+        }
+    }
+    public static class Woman {
+        String name;
+        int age;
+        String address;
+
+        public Woman(String name, int age, String address) {
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+
+        @Override
+        public String toString() {
+            return name + " " + age + " " + address;
+        }
+    }
+}
