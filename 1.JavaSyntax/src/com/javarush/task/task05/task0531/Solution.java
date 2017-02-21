0547f71c5ff0e6d297eddb4f8860e447d32f4622
YuriHeiko <yuriheiko@gmail.com>
2017-02-21 13:41:20 +0200

    task05.task0531 Solution

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0531/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task05.task0531;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Совершенствуем функциональность
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        int c = Integer.parseInt(reader.readLine());
+        int d = Integer.parseInt(reader.readLine());
+        int e = Integer.parseInt(reader.readLine());
+
+        int minimum = min(a, b, c, d, e);
+
+        System.out.println("Minimum = " + minimum);
+    }
+
+
+    public static int min(int a, int b) {
+        return a < b ? a : b;
+    }
+
+    public static int min(int a, int b, int c, int d, int e) {
+        return min(e, min(min(a, b), min(c, d)));
+    }
+
+
+}
