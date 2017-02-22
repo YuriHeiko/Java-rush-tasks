4add9fd0c050a585f08d230ab62bf395ef49d67d
YuriHeiko <yuriheiko@gmail.com>
2017-02-22 20:45:31 +0200

    task06.task0603 Solution

---
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0603/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task06.task0603;
+
+/* 
+По 50 000 объектов Cat и Dog
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        for (int i = 0; i < 50_000; i++) {
+            new Cat();
+            new Dog();
+        }
+    }
+}
+
+class Cat {
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        System.out.println("Cat was destroyed");
+    }
+}
+
+class Dog {
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        System.out.println("Dog was destroyed");
+    }
+}
