5a764cc7be8646897ac0e20adc1cd19a114bf6d9
YuriHeiko <yuriheiko@gmail.com>
2017-02-22 20:56:49 +0200

    task06.task0607 Solution

---
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0607/Cat.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task06.task0607;
+
+/* 
+Классовый счетчик
+*/
+
+public class Cat {
+    static int catCount;
+
+    public Cat() {
+        catCount++;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
