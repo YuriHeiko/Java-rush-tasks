3683916748806cb8a5a0ce6db70db53be9bb6c7b
YuriHeiko <yuriheiko@gmail.com>
2017-02-22 21:08:55 +0200

    task06.task0612 Solution

---
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0612/Calculator.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task06.task0612;
+
+/* 
+Калькулятор
+*/
+
+public class Calculator {
+    public static int plus(int a, int b) {
+        return a + b;
+    }
+
+    public static int minus(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static double division(int a, int b) {
+        return (double) a / b;
+    }
+
+    public static double percent(int a, int b) {
+        return (a * b) / 100;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
