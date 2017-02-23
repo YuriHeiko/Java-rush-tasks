b4dfc3c2803869a4d15d81e5498c618eaf2ed546
YuriHeiko <yuriheiko@gmail.com>
2017-02-23 09:50:59 +0200

    task07.task0704 Solution

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0704/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task07.task0704;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Переверни массив
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[10];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.valueOf(reader.readLine());
+        }
+
+        for (int i = 0; i < arr.length / 2; i++) {
+            int tmp = arr[i];
+            arr[i] = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = tmp;
+        }
+
+        for (int ar : arr) {
+            System.out.println(ar);
+        }
+    }
+}
+
