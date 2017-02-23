d471be33b83024e4aa54086b70355c074d7a8076
YuriHeiko <yuriheiko@gmail.com>
2017-02-23 22:40:58 +0200

    task07.task0714 Solution

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0714/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task07.task0714;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+/* 
+Слова в обратном порядке
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> list = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            list.add(reader.readLine());
+        }
+
+        list.remove(2);
+
+        for (int i = list.size(); i > 0; i--) {
+            System.out.println(list.get(i - 1));
+        }
+    }
+}
+
+
