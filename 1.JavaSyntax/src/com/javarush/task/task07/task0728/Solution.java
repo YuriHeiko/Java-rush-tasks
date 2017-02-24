4e1bd59165b32c36cd4bd10cd40119212e7f3532
YuriHeiko <yuriheiko@gmail.com>
2017-02-24 11:21:02 +0200

    task07.task0728 Solution

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0728/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task07.task0728;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+В убывающем порядке
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = new int[20];
+        for (int i = 0; i < 20; i++) {
+            array[i] = Integer.parseInt(reader.readLine());
+        }
+
+        sort(array);
+
+        for (int x : array) {
+            System.out.println(x);
+        }
+    }
+
+    public static void sort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j] < array[j + 1]) {
+                    int tmp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
+                }
+            }
+        }
+    }
+}
