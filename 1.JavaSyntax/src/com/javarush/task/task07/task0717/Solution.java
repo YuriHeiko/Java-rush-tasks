d8d784f6283a6bb811143b4f2b04f3d9d706000d
YuriHeiko <yuriheiko@gmail.com>
2017-02-24 10:18:04 +0200

    task07.task0717 Solution

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0717/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task07.task0717;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Удваиваем слова
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(reader.readLine());
+        }
+
+        ArrayList<String> result = doubleValues(list);
+
+        for (String s : result) {
+            System.out.println(s);
+        }
+    }
+
+    public static ArrayList<String> doubleValues(ArrayList<String> list) {
+        for (int i = 0; i < list.size(); i += 2) {
+            list.add(i, list.get(i));
+        }
+        return list;
+    }
+}
