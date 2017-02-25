a7b39c1d26151bb980c410fe0d7acfbe16f9d787
YuriHeiko <yuriheiko@gmail.com>
2017-02-25 12:18:55 +0200

    task07.task0809 Solution

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0809/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task08.task0809;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+/* 
+Время для 10 тысяч вставок
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getTimeMsOfInsert(new ArrayList()));
+        System.out.println(getTimeMsOfInsert(new LinkedList()));
+    }
+
+    public static long getTimeMsOfInsert(List list) {
+        long startTime = new Date().getTime();
+
+        insert10000(list);
+
+        return new Date().getTime() - startTime;
+
+    }
+
+    public static void insert10000(List list) {
+        for (int i = 0; i < 10000; i++) {
+            list.add(0, new Object());
+        }
+    }
+}
