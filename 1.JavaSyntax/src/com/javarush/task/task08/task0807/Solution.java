5edeb39634a8e7e5a4792e0633d254b391cd809c
YuriHeiko <yuriheiko@gmail.com>
2017-02-25 11:43:15 +0200

    task07.task0807 Solution

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0807/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task08.task0807;
+
+import java.util.*;
+
+/* 
+LinkedList и ArrayList
+*/
+
+public class Solution {
+    public static Object createArrayList() {
+        return new ArrayList<>();
+
+    }
+
+    public static Object createLinkedList() {
+        return new LinkedList<>();
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
