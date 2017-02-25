22c5a75fa7c843aa1075cc371ada6f4eec5d0053
YuriHeiko <yuriheiko@gmail.com>
2017-02-25 11:21:42 +0200

    task07.task0801 Solution

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0801/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task08.task0801;
+
+/* 
+HashSet из растений
+*/
+
+import java.util.HashSet;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        HashSet<String> set = new HashSet<>();
+        set.add("арбуз");
+        set.add("банан");
+        set.add("вишня");
+        set.add("груша");
+        set.add("дыня");
+        set.add("ежевика");
+        set.add("жень-шень");
+        set.add("земляника");
+        set.add("ирис");
+        set.add("картофель");
+
+        for (String s : set) {
+            System.out.println(s);
+        }
+    }
+}
