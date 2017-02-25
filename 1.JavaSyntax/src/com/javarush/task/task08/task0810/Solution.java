36e295de6aaf7535898c9bf590d9be0a2c43da0b
YuriHeiko <yuriheiko@gmail.com>
2017-02-25 12:19:41 +0200

    task07.task0810 Solution

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0810/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.task.task08.task0810;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+/* 
+Время для 10 тысяч вызовов get
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getTimeMsOfGet(fill(new ArrayList())));
+        System.out.println(getTimeMsOfGet(fill(new LinkedList())));
+    }
+
+    public static List fill(List list) {
+        for (int i = 0; i < 10000; i++) {
+            list.add(new Object());
+        }
+        return list;
+    }
+
+    public static long getTimeMsOfGet(List list) {
+        long startTime = new Date().getTime();
+
+        get10000(list);
+
+        return new Date().getTime() - startTime;
+    }
+
+    public static void get10000(List list) {
+        if (list.isEmpty()) return;
+        int x = list.size() / 2;
+
+        for (int i = 0; i < 10000; i++) {
+            list.get(x);
+        }
+    }
+}
