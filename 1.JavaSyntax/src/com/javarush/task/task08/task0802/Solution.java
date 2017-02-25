bc5d874a84a5d271e43938eb082c545e7df1c2c1
YuriHeiko <yuriheiko@gmail.com>
2017-02-25 11:26:33 +0200

    task07.task0802 Solution

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0802/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task08.task0802;
+
+/* 
+HashMap из 10 пар
+*/
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("арбуз", "ягода");
+        map.put("банан", "трава");
+        map.put("вишня", "ягода");
+        map.put("груша", "фрукт");
+        map.put("дыня", "овощ");
+        map.put("ежевика", "куст");
+        map.put("жень-шень", "корень");
+        map.put("земляника", "ягода");
+        map.put("ирис", "цветок");
+        map.put("картофель", "клубень");
+
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue());
+        }
+    }
+}
