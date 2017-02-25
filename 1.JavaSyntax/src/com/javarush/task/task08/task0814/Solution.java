57525f9634505759a3ac424f4d84ad1abc2da28b
YuriHeiko <yuriheiko@gmail.com>
2017-02-25 14:14:18 +0200

    task07.task0814 Solution

--- 1.JavaSyntax/src/com/javarush/task/task08/task0814/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0814/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task08.task0814;
 
 import java.util.HashSet;
+import java.util.Iterator;
 
 /* 
 Больше 10? Вы нам не подходите
@@ -18,7 +19,12 @@ public class Solution {
     }
 
     public static HashSet<Integer> removeAllNumbersMoreThan10(HashSet<Integer> set) {
-        set.removeIf(v -> v > 10);
+        Iterator<Integer> iterator = set.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next() > 10) {
+                iterator.remove();
+            }
+        }
         return set;
     }
 
