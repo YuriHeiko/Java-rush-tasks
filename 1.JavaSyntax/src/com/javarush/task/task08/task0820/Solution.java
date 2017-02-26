5cd5a8d5465605b380bc69d1cbdd6570813708e0
YuriHeiko <yuriheiko@gmail.com>
2017-02-26 21:59:28 +0200

    task07.task0820 Solution

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0820/Solution.java
@@ -0,0 +1,65 @@
+package com.javarush.task.task08.task0820;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+Множество всех животных
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Set<Cat> cats = createCats();
+        Set<Dog> dogs = createDogs();
+
+        Set<Object> pets = join(cats, dogs);
+        printPets(pets);
+
+        removeCats(pets, cats);
+        printPets(pets);
+    }
+
+    public static Set<Cat> createCats() {
+        HashSet<Cat> result = new HashSet<Cat>();
+
+        for (int i = 0; i < 4; i++) {
+            result.add(new Cat());
+        }
+
+        return result;
+    }
+
+    public static Set<Dog> createDogs() {
+        HashSet<Dog> result = new HashSet<>();
+
+        for (int i = 0; i < 3; i++) {
+            result.add(new Dog());
+        }
+
+        return result;
+    }
+
+    public static Set<Object> join(Set<Cat> cats, Set<Dog> dogs) {
+        Set<Object> set = new HashSet<>(cats);
+
+        set.addAll(dogs);
+
+        return set;
+    }
+
+    public static void removeCats(Set<Object> pets, Set<Cat> cats) {
+        cats.forEach(pets::remove);
+    }
+
+    public static void printPets(Set<Object> pets) {
+        pets.forEach(System.out::println);
+    }
+
+    public static class Cat {
+
+    }
+
+    public static class Dog {
+
+    }
+}
