c93e5b77f1bc479ca092042330ddd631144d639c
YuriHeiko <yuriheiko@gmail.com>
2017-02-26 22:01:37 +0200

    task07.task0821 Solution

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0821/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task08.task0821;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Однофамильцы и тёзки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Map<String, String> map = createPeopleList();
+        printPeopleList(map);
+    }
+
+    public static Map<String, String> createPeopleList() {
+        Map<String, String> map = new HashMap<>();
+
+        for (int i = 0; i < 5; i++) {
+            map.put("Surname", "name" + i);
+        }
+
+        for (int i = 0; i < 5; i++) {
+            map.put("Surname" + i, "name");
+        }
+
+        return map;
+    }
+
+    public static void printPeopleList(Map<String, String> map) {
+        for (Map.Entry<String, String> s : map.entrySet()) {
+            System.out.println(s.getKey() + " " + s.getValue());
+        }
+    }
+}
