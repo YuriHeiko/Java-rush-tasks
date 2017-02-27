a72eaa485df51407850b022a847f7ddd49df4020
YuriHeiko <yuriheiko@gmail.com>
2017-02-27 05:08:04 +0200

    task07.task0824 Solution

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0824/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task08.task0824;
+
+/* 
+Собираем семейство
+*/
+
+import java.util.ArrayList;
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+    }
+
+    public static class Human {
+        String name;
+        boolean sex;
+        int age;
+        ArrayList<Human> children = new ArrayList<>();
+
+        public String toString() {
+            String text = "";
+            text += "Имя: " + this.name;
+            text += ", пол: " + (this.sex ? "мужской" : "женский");
+            text += ", возраст: " + this.age;
+
+            int childCount = this.children.size();
+            if (childCount > 0) {
+                text += ", дети: " + this.children.get(0).name;
+
+                for (int i = 1; i < childCount; i++) {
+                    Human child = this.children.get(i);
+                    text += ", " + child.name;
+                }
+            }
+            return text;
+        }
+    }
+
+}
