94f6648b4508539f6836db75888edc186b4da10f
YuriHeiko <yuriheiko@gmail.com>
2017-02-27 21:45:27 +0200

    task09.task0908 Solution

---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0908/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task09.task0908;
+
+/* 
+Исключение при работе со строками
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        try {
+            String s = null;
+            String m = s.toLowerCase();
+        } catch (NullPointerException e) {
+            System.out.println(e.getClass());
+        }
+    }
+}
