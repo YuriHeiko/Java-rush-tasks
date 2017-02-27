3d23d04749447899a9b657eb1eb6d7476347096c
YuriHeiko <yuriheiko@gmail.com>
2017-02-27 08:28:46 +0200

    task07.task0824 Solution

--- 1.JavaSyntax/src/com/javarush/task/task08/task0824/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0824/Solution.java
@@ -5,10 +5,31 @@ package com.javarush.task.task08.task0824;
 */
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        Human s1 = new Human("f", true, 65);
+        Human s2 = new Human("f", true, 65);
+        Human s3 = new Human("f", true, 65);
+        ArrayList<Human> list = new ArrayList<>(Arrays.asList(s1, s2, s3));
+        Human father = new Human("f", true, 65, list);
+        Human mother = new Human("m", false, 65, list);
+        Human granpa = new Human("granpa", true, 65, new ArrayList<>(Collections.singletonList(father)));
+        Human granpa1 = new Human("granpa", true, 65, new ArrayList<>(Collections.singletonList(mother)));
+        Human granma = new Human("granma", false, 65, new ArrayList<>(Collections.singletonList(father)));
+        Human granma1 = new Human("granma1", false, 65,new ArrayList<>(Collections.singletonList(mother)));
+
+        System.out.println(granpa);
+        System.out.println(granpa1);
+        System.out.println(granma);
+        System.out.println(granma1);
+        System.out.println(father);
+        System.out.println(mother);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
     }
 
     public static class Human {
@@ -16,6 +37,20 @@ public class Solution {
         boolean sex;
         int age;
         ArrayList<Human> children = new ArrayList<>();
+
+        public Human(String name, boolean sex, int age, ArrayList<Human> children) {
+            this.name = name;
+            this.sex = sex;
+            this.age = age;
+            this.children = children;
+        }
+
+        public Human(String name, boolean sex, int age) {
+
+            this.name = name;
+            this.sex = sex;
+            this.age = age;
+        }
 
         public String toString() {
             String text = "";
