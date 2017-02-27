0b0f3a1e79576c1916c7a4cab6af4971e12d13c9
YuriHeiko <yuriheiko@gmail.com>
2017-02-27 21:46:19 +0200

    task09.task0909 Solution

---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0909/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task09.task0909;
+
+/* 
+Исключение при работе с массивами
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        try {
+            int[] m = new int[2];
+            m[8] = 5;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e.getClass());
+        }
+    }
+}
