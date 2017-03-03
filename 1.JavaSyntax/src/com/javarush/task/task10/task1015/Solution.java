66ec7381a93c2d92306ec4321450201982614a96
YuriHeiko <yuriheiko@gmail.com>
2017-03-03 10:34:41 +0200

    task10.task1015 Solution

---
+++ 1.JavaSyntax/src/com/javarush/task/task10/task1015/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task10.task1015;
+
+import java.util.ArrayList;
+
+/* 
+Массив списков строк
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        ArrayList<String>[] arrayOfStringList = createList();
+        printList(arrayOfStringList);
+    }
+
+    public static ArrayList<String>[] createList() {
+        ArrayList<String>[] lists = new ArrayList[5];
+
+        for (int i = 0; i < lists.length; i++) {
+            lists[i] = new ArrayList<>();
+            lists[i].add("1");
+            lists[i].add("2");
+            lists[i].add("3");
+        }
+
+        return lists;
+    }
+
+    public static void printList(ArrayList<String>[] arrayOfStringList) {
+        for (ArrayList<String> list : arrayOfStringList) {
+            for (String s : list) {
+                System.out.println(s);
+            }
+        }
+    }
+}
