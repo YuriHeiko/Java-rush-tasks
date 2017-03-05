80de85f6ef9a0db1b80ebf705b58bcf53a180e1c
YuriHeiko <yuriheiko@gmail.com>
2017-03-05 09:16:12 +0200

    task12.task1207 Solution

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+print(int) и print(Integer)
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print(1);
+        print(new Integer(1000));
+    }
+
+    public static void print(int i) {
+
+    }
+
+    public static void print(Integer integer) {
+
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Условие.jrtc
@@ -0,0 +1,8 @@
+taskKey="com.javarush.task.task12.task1207"
+
+print(int) и print(Integer)
+
+Написать два метода: print(int) и print(Integer).
+Написать такой код в методе main, чтобы вызвались они оба.
+
+
