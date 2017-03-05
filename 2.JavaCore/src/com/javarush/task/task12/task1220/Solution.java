dc529b6551cb4c969f384cac84683682cd2f0f80
YuriHeiko <yuriheiko@gmail.com>
2017-03-05 09:29:59 +0200

    task12.task1220 Solution

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1220/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task12.task1220;
+
+/* 
+Класс Human и интерфейсы Run, Swim
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+
+    public interface Run {
+        public void run();
+    }
+
+    public interface Swim {
+        public void swim();
+    }
+
+
+    public abstract class Human implements Run, Swim {
+
+    }
+}
