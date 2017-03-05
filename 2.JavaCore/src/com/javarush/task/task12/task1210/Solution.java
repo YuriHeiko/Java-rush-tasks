db92b71b5e1cf9334a951b4f77957a20adf20763
YuriHeiko <yuriheiko@gmail.com>
2017-03-05 09:19:12 +0200

    task12.task1210 Solution

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1210/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task12.task1210;
+
+/* 
+Три метода возвращают максимальное из двух переданных в него чисел
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static int max(int a, int b) {
+        return Math.max(a, b);
+    }
+
+    public static long max(long a, long b) {
+        return Math.max(a, b);
+    }
+
+    public static double max(double a, double b) {
+        return Math.max(a, b);
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task12/task1210/Условие.jrtc
@@ -0,0 +1,8 @@
+taskKey="com.javarush.task.task12.task1210"
+
+Три метода возвращают максимальное из двух переданных в него чисел
+
+Написать public static методы: int max(int, int), long max(long, long), double max(double, double).
+Каждый метод должен возвращать максимальное из двух переданных в него чисел.
+
+
