0c192242f05db8ce92274292683e6ddaf3d132eb
YuriHeiko <yuriheiko@gmail.com>
2017-03-05 09:17:07 +0200

    task12.task1208 Solution

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Пять методов print с разными параметрами
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(int i) {
+
+    }
+
+    public static void print(String s) {
+
+    }
+
+    public static void print(String s, int d) {
+
+    }
+
+    public static void print(String s, Solution d) {
+
+    }
+
+    public static void print(String s, double d) {
+
+    }
+
+}
