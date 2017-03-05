55042c1c539fe86697d00d6005a1f3236b240f96
YuriHeiko <yuriheiko@gmail.com>
2017-03-05 09:14:34 +0200

    task12.task1206 Solution

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1206/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task12.task1206;
+
+/* 
+print(int) и print(String)
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(int i) {
+
+    }
+
+    public static void print(String s) {
+
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task12/task1206/Условие.jrtc
@@ -0,0 +1,7 @@
+taskKey="com.javarush.task.task12.task1206"
+
+print(int) и print(String)
+
+Написать два метода: print(int) и print(String).
+
+
