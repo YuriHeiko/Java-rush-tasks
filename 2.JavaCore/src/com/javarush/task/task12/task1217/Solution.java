662b30e0a55532eb6156fd6713624b4318f11846
YuriHeiko <yuriheiko@gmail.com>
2017-03-05 09:24:47 +0200

    task12.task1217 Solution

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1217/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task12.task1217;
+
+/* 
+Интерфейсы Fly, Run, Swim
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface Fly {
+        void takeOff();
+    }
+
+    public interface Run {
+        void go();
+    }
+
+    public interface Swim {
+        void go();
+    }
+}
