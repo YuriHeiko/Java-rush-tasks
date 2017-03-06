2eb467697268caf82b7ed015ce14aa145376ff8d
YuriHeiko <yuriheiko@gmail.com>
2017-03-06 10:58:51 +0200

    task12.task1232 Solution

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1232/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task12.task1232;
+
+/* 
+Нужно добавить в программу новую функциональность
+*/
+
+import java.nio.file.FileSystemException;
+
+public class Solution {
+    public static void main(String[] args) {
+        Pegas horse = new Pegas();
+    }
+
+    public static interface Fly {
+        public void fly();
+    }
+
+    public static class Horse {
+        public void run() {
+
+        }
+    }
+
+    public static class Pegas extends Horse implements Fly {
+        @Override
+        public void fly() {
+
+        }
+    }
+}
