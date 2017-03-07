927c4dedf6a273084a0923b7bba71442c0e44cfc
YuriHeiko <yuriheiko@gmail.com>
2017-03-07 21:31:06 +0200

    task14 lesson 6 Solution

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1408/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task14.task1408;
+
+/* 
+Куриная фабрика
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Hen hen = HenFactory.getHen(Country.BELARUS);
+        hen.getCountOfEggsPerMonth();
+    }
+
+    static class HenFactory {
+
+        static Hen getHen(String country) {
+            Hen hen = null;
+            switch (country) {
+                case Country.RUSSIA:
+                    hen = new RussianHen();
+                    break;
+                case Country.UKRAINE:
+                    hen = new UkrainianHen();
+                    break;
+                case Country.MOLDOVA:
+                    hen = new MoldovanHen();
+                    break;
+                case Country.BELARUS:
+                    hen = new BelarusianHen();
+            }
+            return hen;
+        }
+    }
+
+
+}
