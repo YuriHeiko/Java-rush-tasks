925d043fa07616ae6cc93b444d3389b63c22c773
YuriHeiko <yuriheiko@gmail.com>
2017-03-08 14:56:19 +0200

    task14 lesson 8.2 Solution

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1411/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task14.task1411;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+User, Looser, Coder and Proger
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Person person = null;
+        String key = null;
+
+        while (true) {
+            key = reader.readLine();
+            if (key.equals("user")) {
+                person = new Person.User();
+            } else if (key.equals("loser")) {
+                person = new Person.Loser();
+            } else if (key.equals("coder")) {
+                person = new Person.Coder();
+            } else if (key.equals("proger")) {
+                person = new Person.Proger();
+            } else {
+                break;
+            }
+
+            doWork(person); //вызываем doWork
+
+        }
+    }
+
+    public static void doWork(Person person) {
+        if (person instanceof Person.User) {
+            ((Person.User) person).live();
+        } else if (person instanceof Person.Loser) {
+            ((Person.Loser) person).doNothing();
+        } else if (person instanceof Person.Coder) {
+            ((Person.Coder) person).coding();
+        } else if (person instanceof Person.Proger) {
+            ((Person.Proger) person).enjoy();
+        }
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task14/task1414/Solution.java
@@ -0,0 +1,54 @@
+package com.javarush.task.task14.task1414;
+
+/* 
+MovieFactory
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            String line = reader.readLine();
+            Movie movie = MovieFactory.getMovie(line);
+            if (!(line.equals("soapOpera") || line.equals("cartoon") || line.equals("thriller"))) {
+                break;
+            } else {
+                System.out.println(movie.getClass().getSimpleName());
+            }
+        }
+    }
+
+    static class MovieFactory {
+
+        static Movie getMovie(String key) {
+            Movie movie = null;
+
+            //создание объекта SoapOpera (мыльная опера) для ключа "soapOpera"
+            if ("soapOpera".equals(key)) {
+                movie = new SoapOpera();
+            } else if ("cartoon".equals(key)) {
+                movie = new Cartoon();
+            } else if ("thriller".equals(key)) {
+                movie = new Thriller();
+            }
+
+            return movie;
+        }
+    }
+
+    static abstract class Movie {
+    }
+
+    static class SoapOpera extends Movie {
+    }
+
+    static class Cartoon extends Movie {
+    }
+
+    static class Thriller extends Movie {
+    }
+}
