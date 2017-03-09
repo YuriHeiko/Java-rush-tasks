6dc8f16a6de4a3339db5426e4e083578dd370362
YuriHeiko <yuriheiko@gmail.com>
2017-03-09 22:04:52 +0200

    task15 lesson 4.2 Solution

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1509/Условие.jrtc
@@ -0,0 +1,8 @@
+taskKey="com.javarush.task.task15.task1509"
+
+Мужчина или женщина?
+
+1. Изменить метод printName так, чтобы он выполнялся для man и woman.
+2. Реализация метода printName должна быть одна.
+
+
---
+++ 2.JavaCore/src/com/javarush/task/task15/task1510/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task15.task1510;
+
+/* 
+Все лишнее - прочь!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        add(1, 2);
+    }
+
+    public static void add(int i, int j) {
+        System.out.println("Integer addition");
+    }
+
+    public static void add(int i, double j) {
+        System.out.println("Integer and double addition");
+    }
+
+    public static void add(double i, double j) {
+        System.out.println("Double addition");
+    }
+}
