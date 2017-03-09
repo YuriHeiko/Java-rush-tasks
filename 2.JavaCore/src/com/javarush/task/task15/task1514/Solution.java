6da649748b5b775a8888dc55c4e5ed13fdd49aaa
YuriHeiko <yuriheiko@gmail.com>
2017-03-09 22:16:56 +0200

    task15 lesson 9.1 Solution

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1514/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task15.task1514;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Статики 1
+*/
+
+public class Solution {
+    public static Map<Double, String> labels = new HashMap<Double, String>();
+
+    static {
+        labels.put(1.0, "1");
+        labels.put(2.0, "2");
+        labels.put(3.0, "3");
+        labels.put(4.0, "4");
+        labels.put(5.0, "5");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(labels);
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task15/task1516/Условие.jrtc
@@ -0,0 +1,15 @@
+taskKey="com.javarush.task.task15.task1516"
+
+Значения по-умолчанию
+
+1. Создать 7 public полей класса. Убедитесь, что они инициализируются дефолтными значениями.
+intVar типа int
+doubleVar типа double
+DoubleVar типа Double
+booleanVar типа boolean
+ObjectVar типа Object
+ExceptionVar типа Exception
+StringVar типа String
+2. В методе main вывести их значения в заданном порядке.
+
+
