c2d568238ec7197872e0982325889f82498e3ae4
YuriHeiko <yuriheiko@gmail.com>
2017-03-10 23:05:26 +0200

    task15 lesson 12.7 Solution

--- 2.JavaCore/src/com/javarush/task/task15/task1531/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1531/Solution.java
@@ -3,7 +3,6 @@ package com.javarush.task.task15.task1531;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigDecimal;
 import java.math.BigInteger;
 
 /* 
