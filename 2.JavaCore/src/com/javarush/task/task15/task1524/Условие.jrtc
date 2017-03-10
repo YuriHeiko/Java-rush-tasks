2b0e11d9d374ad6e600eb577ddc505efba5573f2
YuriHeiko <yuriheiko@gmail.com>
2017-03-10 11:00:45 +0200

    task15 lesson 12.3 Solution

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1524/Условие.jrtc
@@ -0,0 +1,19 @@
+taskKey="com.javarush.task.task15.task1524"
+
+Порядок загрузки переменных
+
+Разберись, что и в какой последовательности инициализируется. Поставь брейкпойнты и используй дебаггер.
+Исправить порядок инициализации данных так, чтобы результат был следующим:
+static void init()
+Static block
+public static void main
+non-static block
+static void printAllFields
+0
+null
+Solution constructor
+static void printAllFields
+6
+First name
+
+
---
+++ 2.JavaCore/src/com/javarush/task/task15/task1525/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task15.task1525;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Файл в статическом блоке
+*/
+
+public class Solution {
+    public static List<String> lines = new ArrayList<String>();
+
+    static {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(Statics.FILE_NAME));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lines);
+    }
+}
