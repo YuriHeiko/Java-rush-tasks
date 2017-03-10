755828364d9d3b5f023f2a3989442b3127820e08
YuriHeiko <yuriheiko@gmail.com>
2017-03-10 21:54:51 +0200

    task15 lesson 12.5 Solution

--- 2.JavaCore/src/com/javarush/task/task15/task1527/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1527/Solution.java
@@ -1,16 +1,48 @@
 package com.javarush.task.task15.task1527;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
 
 /* 
 Парсер реквестов
+
+    http://javarush.ru/alpha/index.html?lvl=15&view&name=Amigo&obj=20&obj=Test=ff&obj=1.232&obj=1,1212
+    lvl view name obj obj obj obj
+    double 20.0
+    String Test
+    double 1.232
+    String 1,1212
 */
 
 public class Solution {
     public static void main(String[] args) {
-        //add your code here
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+            List<String> values = new LinkedList<>();
+            for (String line : reader.readLine().split("\\?")[1].split("&")) {
+                String[] splitParams = line.split("=");
+                if (splitParams[0].equals("obj")) {
+                    values.add(splitParams[1]);
+                }
+                System.out.print(splitParams[0] + " ");
+            }
+            System.out.println();
+
+            for (String value : values) {
+                try {
+                    alert(Double.parseDouble(value));
+                } catch (NumberFormatException e) {
+                    alert(value);
+                }
+            }
+
+            reader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void alert(double value) {
