2b384814e3edf39f7e63d9798492203ed5a6659b
YuriHeiko <yuriheiko@gmail.com>
2017-03-13 15:25:14 +0200

    task16 lesson 10.7 Solution

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1629/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task16.task1629;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static volatile BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws InterruptedException {
+        Read3Strings t1 = new Read3Strings();
+        Read3Strings t2 = new Read3Strings();
+
+        t1.start();
+        t1.join();
+        t2.start();
+        t2.join();
+
+        t1.printResult();
+        t2.printResult();
+    }
+
+    public static class Read3Strings extends Thread {
+        private String result;
+
+        @Override
+        public void run() {
+            try {
+                result = reader.readLine() + " " + reader.readLine() + " " + reader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        public void printResult() {
+            System.out.println(result);
+        }
+    }
+
+    //add your code here - добавьте код тут
+}
