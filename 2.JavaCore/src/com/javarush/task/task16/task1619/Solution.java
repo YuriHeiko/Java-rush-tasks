5acec50414c86b5160db9ba5e52355b4e722218b
YuriHeiko <yuriheiko@gmail.com>
2017-03-13 14:13:28 +0200

    task16 lesson 10.2 Solution

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1619/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task16.task1619;
+
+/* 
+А без interrupt слабо?
+*/
+
+public class Solution {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(new TestThread());
+        t.start();
+        Thread.sleep(3000);
+        ourInterruptMethod();
+    }
+
+    public static void ourInterruptMethod() {
+        TestThread.stopThread = true;
+    }
+
+    public static class TestThread implements Runnable {
+        public static boolean stopThread = false;
+
+        public void run() {
+            while (!stopThread) {
+                try {
+                    System.out.println("he-he");
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                }
+            }
+        }
+    }
+}
