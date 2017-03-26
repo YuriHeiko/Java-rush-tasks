14c7d6d2c95699eddddfde7bf07dac9fcdb1672e
YuriHeiko <yuriheiko@gmail.com>
2017-03-26 11:14:42 +0300

    task18 lesson 11.7 Solution

--- 2.JavaCore/src/com/javarush/task/task18/task1826/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1826/Solution.java
@@ -4,9 +4,26 @@ package com.javarush.task.task18.task1826;
 Шифровка
 */
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Solution {
     public static void main(String[] args) {
+//        args = new String[]{"-e", "d:\\_JavaRush\\_Test\\Dear Sir or Madam.txt", "d:\\_JavaRush\\_Test\\temp.txt"};
+//        args = new String[]{"-d", "d:\\_JavaRush\\_Test\\temp.txt", "d:\\_JavaRush\\_Test\\Dear Sir or Madam.txt"};
 
+        encryptDecryptFile(args[1], args[2]);
     }
 
+    public static void encryptDecryptFile(String sourceFileName, String destinationFileName) {
+        try (FileInputStream fis = new FileInputStream(sourceFileName);
+             FileOutputStream fos = new FileOutputStream(destinationFileName)) {
+            while (fis.available() > 0) {
+                fos.write(fis.read() ^ 0b11111111);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
