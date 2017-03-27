5e4878f438be1aabe125b625d368d9c8421b79ef
YuriHeiko <yuriheiko@gmail.com>
2017-03-27 22:35:51 +0300

    task19 lesson 3.2 Solution

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1902/AdapterFileOutputStream.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task19.task1902;
+
+/* 
+Адаптер
+*/
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class AdapterFileOutputStream implements AmigoStringWriter {
+    private FileOutputStream fileOutputStream;
+
+    public static void main(String[] args) {
+
+    }
+
+    public AdapterFileOutputStream(FileOutputStream fileOutputStream) {
+        this.fileOutputStream = fileOutputStream;
+    }
+
+    @Override
+    public void flush() throws IOException {
+        fileOutputStream.flush();
+    }
+
+    @Override
+    public void writeString(String s) throws IOException {
+        fileOutputStream.write(s.getBytes());
+    }
+
+    @Override
+    public void close() throws IOException {
+        fileOutputStream.close();
+    }
+}
+
