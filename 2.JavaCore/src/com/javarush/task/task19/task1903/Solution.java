73e58656ff0459cf214fddb551b828869db4681c
YuriHeiko <yuriheiko@gmail.com>
2017-03-28 11:57:38 +0300

    task19 lesson 3.3 Solution

--- 2.JavaCore/src/com/javarush/task/task19/task1903/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1903/Solution.java
@@ -10,11 +10,45 @@ import java.util.Map;
 public class Solution {
     public static Map<String, String> countries = new HashMap<String, String>();
 
+    static{
+        countries.put("UA", "Ukraine");
+        countries.put("RU", "Russia");
+        countries.put("CA", "Canada");
+    }
+
     public static void main(String[] args) {
 
     }
 
-    public static class IncomeDataAdapter {
+    public static class IncomeDataAdapter implements Customer, Contact {
+        private IncomeData data;
+
+        public IncomeDataAdapter(IncomeData incomeData) {
+            this.data = incomeData;
+        }
+
+        @Override
+        public String getCompanyName() {
+            return data.getCompany();
+        }
+
+        @Override
+        public String getCountryName() {
+            return countries.get(data.getCountryCode());
+        }
+
+        @Override
+        public String getName() {
+            return String.format("%s, %s", data.getContactLastName(), data.getContactFirstName());
+        }
+
+        @Override
+        public String getPhoneNumber() {
+            String phone = String.format("%010d", data.getPhoneNumber());
+            String result = "+" + data.getCountryPhoneCode() + "(" + phone.substring(0,3) + ")" +
+                    phone.substring(3,6) + "-" + phone.substring(6,8) + "-" + phone.substring(8);
+            return result ;
+        }
     }
 
 
