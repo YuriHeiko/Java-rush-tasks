a80524f5b66a28f79d710847ddf23d0ffb9d03d3
YuriHeiko <yuriheiko@gmail.com>
2017-03-30 08:47:33 +0300

    task19 lesson 3.5 Solution

--- 2.JavaCore/src/com/javarush/task/task16/task1603/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1603/Solution.java
@@ -11,11 +11,9 @@ public class Solution {
     public static volatile List<Thread> list = new ArrayList<Thread>(5);
 
     public static void main(String[] args) {
-        list.add(new Thread(new SpecialThread()));
-        list.add(new Thread(new SpecialThread()));
-        list.add(new Thread(new SpecialThread()));
-        list.add(new Thread(new SpecialThread()));
-        list.add(new Thread(new SpecialThread()));
+        for (int i = 0; i < 5; i++) {
+            list.add(new Thread(new SpecialThread()));
+        }
     }
 
     public static class SpecialThread implements Runnable {
