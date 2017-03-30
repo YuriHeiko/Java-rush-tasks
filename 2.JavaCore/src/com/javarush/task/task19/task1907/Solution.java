5a16597babfecf0d67e7d90359d9876d83c3500e
YuriHeiko <yuriheiko@gmail.com>
2017-03-30 11:47:45 +0300

    task19 lesson 5.3 Solution

--- 2.JavaCore/src/com/javarush/task/task19/task1907/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1907/Solution.java
@@ -2,11 +2,6 @@ package com.javarush.task.task19.task1907;
 
 /* 
 Считаем слово
-
-
-d:\_JavaRush\_Test\19.05.02.txt
-
-
 */
 
 import java.io.BufferedReader;
@@ -15,38 +10,29 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Solution {
-    public static void main(String[] args) {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-             FileReader fileReader = new FileReader(reader.readLine())) {
-            int nextChar;
-            StringBuilder builder = new StringBuilder();
-            while ((nextChar = fileReader.read()) != -1) {
-                builder.append((char)nextChar);
-            }
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String file = reader.readLine();
+        FileReader stream = new FileReader(file);
 
-            String[] words = builder.toString().replaceAll("[\\p{Punct}+]", " ").split(" ");
-            int count = 0;
-            for (String word : words) {
-                if (word.equalsIgnoreCase("world")) {
-                    count++;
-                }
-            }
-
-            System.out.println(count);
-
-        /*        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-             Scanner scanner = new Scanner(new FileReader(reader.readLine())).useDelimiter("[\\p{Punct} ]")) {
-            int count = 0;
-            while (scanner.hasNext()) {
-                if (scanner.next().equalsIgnoreCase("world")) {
-                    count++;
-                }
-            }
-
-            System.out.println(count);*/
-        } catch (IOException e) {
-            e.printStackTrace();
+        String line = "";
+        while (stream.ready()){
+            line += (char)stream.read();
         }
+
+        line = line.replaceAll("[\\p{Punct}+|\\s]"," ");
+        String[] lineArr = line.split(" ");
+        int counter = 0;
+        for (int i = 0; i < lineArr.length; i++) {
+            if (lineArr[i].equals("world")){
+                counter = counter + 1;
+            }
+        }
+
+        System.out.println(counter);
+
+        reader.close();
+        stream.close();
     }
 }
 
