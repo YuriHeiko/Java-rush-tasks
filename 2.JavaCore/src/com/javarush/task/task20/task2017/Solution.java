2eb4763ee09b788c1af40069dbd368fe15969690
YuriHeiko <yuriheiko@gmail.com>
2017-04-07 22:41:19 +0300

    task20 lesson 10.2 Solution

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2017/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task20.task2017;
+
+import java.io.ObjectInputStream;
+import java.io.Serializable;
+
+/* 
+Десериализация
+*/
+public class Solution {
+    public A getOriginalObject(ObjectInputStream objectStream) {
+        try {
+            return (A) objectStream.readObject();
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
+    public class A implements Serializable{
+    }
+
+    public class B extends A {
+        public B() {
+            System.out.println("inside B");
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
