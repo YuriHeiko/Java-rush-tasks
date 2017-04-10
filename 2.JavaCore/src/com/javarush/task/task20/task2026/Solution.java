0f96586f126e9432f57c8a84964e0c1dceecca1d
YuriHeiko <yuriheiko@gmail.com>
2017-04-10 16:10:25 +0300

    task20 lesson 10.10 Solution

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2026/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task20.task2026;
+
+/* 
+Алгоритмы-прямоугольники
+*/
+public class Solution {
+    public static void main(String[] args) {
+        byte[][] a = new byte[][]{
+                {1, 1, 0, 0},
+                {1, 1, 0, 0},
+                {1, 1, 0, 0},
+                {1, 1, 0, 1}
+        };
+        int count = getRectangleCount(a);
+        System.out.println("Count = " + count + ". Должно быть 2");
+        a = new byte[][]{
+                {1, 1, 0, 0},
+                {1, 1, 0, 0},
+                {0, 0, 0, 0},
+                {1, 1, 0, 1}
+        };
+        count = getRectangleCount(a);
+        System.out.println("Count = " + count + ". Должно быть 3");
+        a = new byte[][]{
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {1, 1, 0, 0},
+                {1, 1, 0, 0}
+        };
+        count = getRectangleCount(a);
+        System.out.println("Count = " + count + ". Должно быть 1");
+        a = new byte[][]{
+                {1, 1, 0, 0, 0},
+                {1, 1, 0, 1, 1},
+                {1, 1, 0, 0, 0},
+                {1, 1, 0, 0, 1},
+                {0, 0, 0, 0, 1}
+        };
+        count = getRectangleCount(a);
+        System.out.println("Count = " + count + ". Должно быть 3");
+    }
+
+    public static int getRectangleCount(byte[][] a) {
+        return 0;
+    }
+}
