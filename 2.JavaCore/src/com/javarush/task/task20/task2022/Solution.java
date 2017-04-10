e893f56e0c55a93b9a7a85e946bc25067cbd6d7c
YuriHeiko <yuriheiko@gmail.com>
2017-04-10 14:27:37 +0300

    task20 lesson 10.7 Solution

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2022/Solution.java
@@ -0,0 +1,59 @@
+package com.javarush.task.task20.task2022;
+
+import java.io.*;
+
+/* 
+Переопределение сериализации в потоке
+*/
+public class Solution implements Serializable, AutoCloseable {
+    private transient FileOutputStream stream;
+    String fileName;
+
+    public Solution(String fileName) throws FileNotFoundException {
+        this.stream = new FileOutputStream(fileName);
+        this.fileName = fileName;
+    }
+
+    public void writeObject(String string) throws IOException {
+        stream.write(string.getBytes());
+        stream.write("\n".getBytes());
+        stream.flush();
+    }
+
+    private void writeObject(ObjectOutputStream out) throws IOException {
+        out.defaultWriteObject();
+//        out.close();
+    }
+
+    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+        in.defaultReadObject();
+        stream = new FileOutputStream(this.fileName, true);
+//        in.close();
+    }
+
+    @Override
+    public void close() throws Exception {
+        System.out.println("Closing everything!");
+        stream.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        Solution solution1 = new Solution("d:/ser.txt");
+        solution1.writeObject("Hi1");
+        solution1.close();
+
+        FileOutputStream fileOutputStream = new FileOutputStream("d:/ser.txt");
+        ObjectOutputStream outputStream = new ObjectOutputStream(fileOutputStream);
+        outputStream.writeObject(solution1);
+        outputStream.flush();
+        outputStream.close();
+
+        FileInputStream fileInputStream = new FileInputStream("d:/ser.txt");
+        ObjectInputStream inputStream = new ObjectInputStream(fileInputStream);
+        Solution solution2 = (Solution) inputStream.readObject();
+        inputStream.close();
+        solution2.writeObject("Hi2");
+        solution2.writeObject("Hi3");
+        solution2.close();
+    }
+}
