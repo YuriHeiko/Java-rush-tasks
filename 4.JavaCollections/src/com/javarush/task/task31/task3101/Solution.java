9a9f9c482a6e1be63ba05de81f7e0f4db8364976
YuriHeiko <yuriheiko@gmail.com>
2017-04-14 19:33:50 +0300

    task31 lesson 01.01 Solution

--- 4.JavaCollections/src/com/javarush/task/task31/task3101/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3101/Solution.java
@@ -1,56 +1,49 @@
 package com.javarush.task.task31.task3101;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.TreeSet;
 
 /*
 Проход по дереву файлов
 */
+
 public class Solution {
-    public static TreeSet<String> set = new TreeSet<>(Comparator.comparing(f -> f.substring(f.lastIndexOf("\\"))));
-
     public static void main(String[] args) {
-        args = new String[]{"d:\\_JavaRush\\_Test_1", "d:\\_JavaRush\\_Test\\temp.txt"};
+        args = new String[]{"d:\\_JavaRush\\_Test_1", "d:\\_JavaRush\\temp.txt"};
 
-        File file = new File(args[0]);
-
-        readDirectoryAndFillSet(file);
-
-        File forWrite = new File("allFilesContent.txt");
-
-        FileUtils.renameFile(new File(args[1]), forWrite);
-
-        try (FileOutputStream fos = new FileOutputStream(forWrite)) {
-            writeAllFiles(fos);
+        File dir = new File(args[0]);
+        File resultFileAbsolutePath = new File(args[1]);
+        File dest = new File(resultFileAbsolutePath.getParent() + "/allFilesContent.txt");
+        FileUtils.renameFile(resultFileAbsolutePath, dest);
+        try (FileOutputStream writer = new FileOutputStream(dest)) {
+            ArrayList<File> list = new ArrayList<>();
+            Files.walkFileTree(dir.toPath(), new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    if (file.toFile().length() > 50) FileUtils.deleteFile(file.toFile());
+                    else list.add(file.toFile());
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+            list.sort(Comparator.comparing(File::getName));
+            for (File file : list) {
+                System.out.println(file);
+                FileReader reader = new FileReader(file);
+                while (reader.ready()) writer.write(reader.read());
+                reader.close();
+                writer.write('\n');
+            }
         } catch (IOException e) {
             e.printStackTrace();
-        }
-    }
-
-    public static void writeAllFiles(FileOutputStream fos) {
-        for (String s : set) {
-            System.out.println(s);
-            try (FileInputStream fis = new FileInputStream(s)) {
-                byte[] bytes = new byte[fis.available()];
-                fis.read(bytes);
-                fos.write(bytes);
-                fos.write("\\n".getBytes());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public static void readDirectoryAndFillSet(File file) {
-        for (File f : file.listFiles()) {
-            if (f.isDirectory()) {
-                readDirectoryAndFillSet(f);
-            } else if (f.length() > 50) {
-                FileUtils.deleteFile(f);
-            } else {
-                set.add(f.getAbsolutePath());
-            }
         }
     }
 
